c9ccdd82d4c1926d5ac65f0af210d30d52fd64ba
Maeriel Benedicto <N00150817@student.iadt.ie>
2019-10-19 20:34:45 +0100

    Initial Commit

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+# @Author: maerielbenedicto
+# @Date:   2019-08-27T22:26:48+01:00
+# @Last modified by:   maerielbenedicto
+# @Last modified time: 2019-10-19T20:01:31+01:00
+
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('homepage');
+});
